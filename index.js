a3b54c12ac9b8a466e417f3640973d14a252c8e2
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-10-29 20:52:49 +0700

    ketemu konsep nya. alhamdulillah ...

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+
+const express = require('express');
 const { Server } = require('socket.io');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
 const server = createServer(app);
 const ioScanning = new Server(server, { path: '/scanning' });
+
+let id = '';
+
+ioScanning.use((socket, next) => {
+  console.log('check middleware enging');
+  id = (socket.request.headers.cookie).split('=')[1];
+  next();
+});
+
+// disini ada join room berdasarkan id dari gate id.
+
+ioScanning.on('connection', (socket) => {
+  console.log('id =>>>', id);
+  socket.join(id);
+
+  console.log('pass middleware, id =>>> ' + id);
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.get('/first-page', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
@@ -23,23 +49,19 @@ app.get('/scanner', (req, res) => {
   res.sendFile(join(__dirname, 'scanner.html'));
 });
 
-ioScanning.engine.use((req, res, next) => {
-  
-});
+app.post('/scanning', (req, res) => {
+  const token = req.cookies.token;
+  const data = req.body;
+  console.log('token =>>>', token);
 
-ioScanning.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  if (data.isValid === '1') {
+    ioScanning.to(token).emit('ticket-valid', data);
+  } else {
+    ioScanning.to(token).emit('ticket-not-valid', data);
+  }
 
-  socket.on('request-scanner', (data) => {
-    if (data.data === 'some valid data') {
-      console.log('data =>>>', data.data);
-      ioScanning.emit('ticket-valid', data);
-    } else {
-      ioScanning.emit('ticket-not-valid', data);
-    }
+  res.json({
+    message: 'ok'
   });
 });
 
